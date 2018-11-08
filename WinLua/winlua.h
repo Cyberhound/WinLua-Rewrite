358b982495e3317961930d70122226b730bd0287
Cyberhound <mikeyiscool123456789@gmail.com>
2018-11-08 08:57:47 -0500

    Creation

---
+++ WinLua/winlua.h
@@ -0,0 +1,21 @@
+#ifndef WINLUA_H_V1
+#define WINLUA_H_V1
+#include <Windows.h>
+
+class winlua {
+private:
+	struct imgui_form {
+	    
+	}
+	
+	struct winapi_form {
+	    
+	}
+	
+	struct control {
+	    
+	}
+public:
+	
+}
+#endif
